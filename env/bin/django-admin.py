b3ecf5c23944c185466f3a3d7753b7f043d44dd7
Vitali Kremez <vkremez@flashpoint-intel.com>
2017-01-08 17:14:27 -0500

    my first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mj/Desktop/chartkick.py-master/examples/demo/myenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
